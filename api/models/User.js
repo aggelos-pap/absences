8c4594fc4c4950bdd50274ada6249a2b4d7cd064
Angelos Papadakis <theakero@gmail.com>
2023-04-20 21:35:51 +0300

    Better seperation with controllers

--- api/models/User.js
+++ api/models/User.js
@@ -2,6 +2,15 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
     name: {
       type: String,
       required: true,
