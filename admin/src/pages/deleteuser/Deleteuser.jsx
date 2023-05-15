e28b41d2f8c39dada366be3d40f07003236b0493
Aggelos <theakero@gmail.com>
2023-05-15 11:24:02 +0300

    test

--- admin/src/pages/deleteuser/Deleteuser.jsx
+++ admin/src/pages/deleteuser/Deleteuser.jsx
@@ -1,3 +1,76 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import Sidebar from "../../components/sidebar/Sidebar";
+// import Navbar from "../../components/navbar/Navbar";
+// import "./deleteuser.scss";
+
+// const DeleteUser = () => {
+//   const [users, setUsers] = useState([]);
+//   const [selectedUser, setSelectedUser] = useState("");
+
+//   useEffect(() => {
+//     fetchUsers();
+//   }, []);
+
+//   const fetchUsers = async () => {
+//     try {
+//       const response = await axios.get("http://localhost:8800/api/users");
+//       setUsers(response.data);
+//     } catch (error) {
+//       console.error("Error fetching users:", error);
+//     }
+//   };
+
+//   const handleUserSelect = (event) => {
+//     setSelectedUser(event.target.value);
+//   };
+
+//   const handleDeleteUser = async () => {
+//     try {
+//       await axios.delete(`http://localhost:8800/api/users/${selectedUser}`);
+//       // Handle success, e.g., show a success message or update the user list
+//     } catch (error) {
+//       console.error("Error deleting user:", error);
+//       // Handle error, e.g., show an error message
+//     }
+//   };
+
+//   return (
+//     <div className="new">
+//       <Sidebar />
+//       <div className="newContainer">
+//         <Navbar />
+
+//         <div className="left">
+//           <h1>Delete User</h1>
+
+//           <div className="right">
+//             <div className="form">
+//               <select value={selectedUser} onChange={handleUserSelect}>
+//                 <option value="">Select a user</option>
+//                 {users.map((user) => (
+//                   <option key={user._id} value={user._id}>
+//                     {user.name}
+//                   </option>
+//                 ))}
+//               </select>
+//             </div>
+//             <button
+//               className="button"
+//               onClick={handleDeleteUser}
+//               disabled={!selectedUser}
+//             >
+//               Delete User
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DeleteUser;
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Sidebar from "../../components/sidebar/Sidebar";
@@ -7,6 +80,7 @@ import "./deleteuser.scss";
 const DeleteUser = () => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState("");
+  const [selectedUserDetails, setSelectedUserDetails] = useState(null);
 
   useEffect(() => {
     fetchUsers();
@@ -22,7 +96,10 @@ const DeleteUser = () => {
   };
 
   const handleUserSelect = (event) => {
-    setSelectedUser(event.target.value);
+    const userId = event.target.value;
+    setSelectedUser(userId);
+    const userDetails = users.find((user) => user._id === userId);
+    setSelectedUserDetails(userDetails);
   };
 
   const handleDeleteUser = async () => {
@@ -40,8 +117,10 @@ const DeleteUser = () => {
       <Sidebar />
       <div className="newContainer">
         <Navbar />
+
         <div className="left">
           <h1>Delete User</h1>
+
           <div className="right">
             <div className="form">
               <select value={selectedUser} onChange={handleUserSelect}>
@@ -53,6 +132,14 @@ const DeleteUser = () => {
                 ))}
               </select>
             </div>
+            {selectedUserDetails && (
+              <div className="userCard">
+                <h2>{selectedUserDetails.name}</h2>
+                <p>Username: {selectedUserDetails.username}</p>
+                <p>Email: {selectedUserDetails.email}</p>
+                {/* Add more user details as needed */}
+              </div>
+            )}
             <button
               className="button"
               onClick={handleDeleteUser}
--- admin/src/pages/edituser/Edituser.jsx
+++ admin/src/pages/edituser/Edituser.jsx
@@ -1,3 +1,198 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import "./edituser.scss";
+// import Sidebar from "../../components/sidebar/Sidebar";
+// import Navbar from "../../components/navbar/Navbar";
+// import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
+
+// const EditUser = () => {
+//   const [users, setUsers] = useState([]);
+//   const [selectedUser, setSelectedUser] = useState(null);
+//   const [isSelected, setIsSelected] = useState(false);
+//   const [formData, setFormData] = useState({
+//     username: "",
+//     password: "",
+//     name: "",
+//     email: "",
+//     am: "",
+//   });
+
+//   useEffect(() => {
+//     getUsers();
+//   }, []);
+
+//   const getUsers = async () => {
+//     try {
+//       const response = await axios.get("http://localhost:8800/api/users");
+//       setUsers(response.data);
+//     } catch (error) {
+//       console.error("Error fetching users:", error);
+//     }
+//   };
+
+//   const handleCheckboxChange = (e) => {
+//     setIsSelected(e.target.checked);
+//     setSelectedUser(null);
+//     setFormData({
+//       username: "",
+//       password: "",
+//       name: "",
+//       email: "",
+//       am: "",
+//     });
+//   };
+
+//   const handleUserSelect = (user) => {
+//     setSelectedUser(user);
+//     setFormData({
+//       username: user.username,
+//       password: "",
+//       name: user.name,
+//       email: user.email,
+//       am: user.am,
+//     });
+//   };
+
+//   const handleInputChange = (e) => {
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       const updatedUser = {
+//         ...formData,
+//       };
+
+//       await axios.put(
+//         `http://localhost:8800/api/users/${selectedUser._id}`,
+//         updatedUser
+//       );
+
+//       // Perform any necessary actions after successful user update
+//       // ...
+
+//       // Clear form data and reset selection
+//       setFormData({
+//         username: "",
+//         password: "",
+//         name: "",
+//         email: "",
+//         am: "",
+//       });
+//       setSelectedUser(null);
+//       setIsSelected(false);
+//     } catch (error) {
+//       console.error("Error updating user:", error);
+//     }
+//   };
+
+//   return (
+//     <div className="new">
+//       <Sidebar />
+//       <div className="newContainer">
+//         <Navbar />
+//         <div className="top">
+//           <h1>Edit User</h1>
+//         </div>
+//         <div className="bottom">
+//           <div className="left">
+//             <p>Select user to edit</p>
+
+//             <ul style={{ listStyle: "none" }} className="userList">
+//               {users.map((user) => (
+//                 <li
+//                   key={user._id}
+//                   className={selectedUser?._id === user._id ? "selected" : ""}
+//                   onClick={() => handleUserSelect(user)}
+//                 >
+//                   {user.name}
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//           <div className="right">
+//             {selectedUser && (
+//               <form onSubmit={handleSubmit}>
+//                 <div className="formInput">
+//                   <label htmlFor="password">Password:</label>
+//                   <input
+//                     type="password"
+//                     id="password"
+//                     name="password"
+//                     value={formData.password}
+//                     onChange={handleInputChange}
+//                   />
+//                 </div>
+//                 <div className="formInput">
+//                   <label htmlFor="isSelected">Select User:</label>
+//                   <input
+//                     type="checkbox"
+//                     id="isSelected"
+//                     name="isSelected"
+//                     checked={isSelected}
+//                     onChange={handleCheckboxChange}
+//                   />
+//                 </div>
+//                 <div className="formInput">
+//                   <label htmlFor="username">Username:</label>
+//                   <input
+//                     type="text"
+//                     id="username"
+//                     name="username"
+//                     value={formData.username}
+//                     onChange={handleInputChange}
+//                     disabled={!isSelected}
+//                   />
+//                 </div>
+//                 <div className="formInput">
+//                   <label htmlFor="name">Name:</label>
+//                   <input
+//                     type="text"
+//                     id="name"
+//                     name="name"
+//                     value={formData.name}
+//                     onChange={handleInputChange}
+//                     disabled={!isSelected}
+//                   />
+//                 </div>
+//                 <div className="formInput">
+//                   <label htmlFor="email">Email:</label>
+//                   <input
+//                     type="email"
+//                     id="email"
+//                     name="email"
+//                     value={formData.email}
+//                     onChange={handleInputChange}
+//                     disabled={!isSelected}
+//                   />
+//                 </div>
+//                 <div className="formInput">
+//                   <label htmlFor="am">AM:</label>
+//                   <input
+//                     type="text"
+//                     id="am"
+//                     name="am"
+//                     value={formData.am}
+//                     onChange={handleInputChange}
+//                     disabled={!isSelected}
+//                   />
+//                 </div>
+//                 <button type="submit" disabled={!isSelected}>
+//                   Update User
+//                 </button>
+//               </form>
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default EditUser;
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./edituser.scss";
@@ -100,17 +295,21 @@ const EditUser = () => {
           <div className="left">
             <p>Select user to edit</p>
 
-            <ul style={{ listStyle: "none" }} className="userList">
+            <select
+              value={selectedUser?.name || ""}
+              onChange={(e) =>
+                handleUserSelect(
+                  users.find((user) => user.name === e.target.value)
+                )
+              }
+            >
+              <option value="">Select User</option>
               {users.map((user) => (
-                <li
-                  key={user._id}
-                  className={selectedUser?._id === user._id ? "selected" : ""}
-                  onClick={() => handleUserSelect(user)}
-                >
+                <option key={user._id} value={user.name}>
                   {user.name}
-                </li>
+                </option>
               ))}
-            </ul>
+            </select>
           </div>
           <div className="right">
             {selectedUser && (
